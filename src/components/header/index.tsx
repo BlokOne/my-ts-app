5a698d27a6cdd52767462f92b447fae5679df425
BlokOne <90694470+BlokOne@users.noreply.github.com>
2024-03-29 20:13:06 +0300

    fix img

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,15 @@
 import Button from "../../shared/button";
 import { HeaderStyle } from "./style";
 import Navigation from "./ui/navigation";
+import logo from '../../../public/logo.svg'
 
 export default function Header() {
   return (
     <header
       style={HeaderStyle}
     >
-      <img src={`public/logo.svg`} />
-      <Navigation  />
+      <img src={logo} />
+      <Navigation />
       <Button width="175px">
         Join
       </Button>
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,22 +1,27 @@
 import Button from "../../shared/button"
 import { titleStyle } from "./style"
 
+import texts from '../../../public/1/texts.svg'
+import bigLogo from '../../../public/1/bigLogo.svg'
+import mobile from '../../../public/1/mobile.svg'
+
+
 function Title() {
   return (
     <>
       <h1 style={titleStyle}>T-Project</h1>
-      <img style={{ maxWidth: "778.93px", marginBottom: '15px' }} src="public/1/texts.svg" />
+      <img style={{ maxWidth: "778.93px", marginBottom: '15px' }} src={texts} />
       <Button width="326px">
         Buy TTTU on MEXC
       </Button>
       <div style={{ display: 'flex', gap: "167px", marginTop: '72px', alignItems: 'center', marginBottom: '99px' }}>
         <div style={{ display: 'flex', flexDirection: 'column', gap: '109px', }}>
-          <img src="public/1/bigLogo.svg" />
+          <img src={bigLogo} />
           <Button width="396px" arrow={true}>
             Start T-App in the Telegram
           </Button>
         </div>
-        <img src="public/1/mobile.svg" />
+        <img src={mobile} />
       </div>
     </>
   )
