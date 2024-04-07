61761975d565dab6e0aec2d6e7820539e5d52b84
BlokOne <90694470+BlokOne@users.noreply.github.com>
2024-04-07 22:01:17 +0300

    fix

--- src/components/roadmap/api.tsx
+++ src/components/roadmap/api.tsx
@@ -1,4 +1,4 @@
-const ROADMAP_API: string = 'http://185.209.31.112:8080/api/v2/dashboard/stage';
+const ROADMAP_API: string = 'https://shiner-powerful-coyote.ngrok-free.app/api/v2/dashboard/stage';
 
 function getRoadmapActiveStage() {
     return fetch(ROADMAP_API, { method: 'GET' });
