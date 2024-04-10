222d26a45c740bfe35113e5bb6a860746c44a835
BlokOne <90694470+BlokOne@users.noreply.github.com>
2024-04-10 10:08:46 +0300

    123

--- src/components/referral/index.tsx
+++ src/components/referral/index.tsx
@@ -1,4 +1,6 @@
 import imgReferral from '../../../public/9/icons.svg'
+import imgReferral2 from '../../../public/9/iconsMob.svg'
+
 import { useMediaQuery } from '../../media/useMedia'
 
 function Referral() {
@@ -6,12 +8,18 @@ function Referral() {
   return (
     <>
       {
-        !isMobile && <div style={{ display: 'flex', flexDirection: 'column', gap: '106px', alignItems: 'center', marginBottom: '254px' }}>
+        !isMobile ? <div style={{ display: 'flex', flexDirection: 'column', gap: '106px', alignItems: 'center', marginBottom: '254px' }}>
           <p style={{ fontWeight: 'bold', fontSize: '60px', color: 'white' }}>
             Referral program
           </p>
           <img src={imgReferral} />
-        </div>
+        </div> :
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '106px', alignItems: 'center', justifyContent: 'center', marginBottom: '120px' }}>
+            <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '60px', color: 'white' }}>
+              Referral program
+            </p>
+            <img src={imgReferral2} />
+          </div>
       }
     </>
 
